2030e4a682f55b7a1c715718d77fac5d0426d342
Julio Chavez <julio.chavez.dorado@gmail.com>
2023-11-30 09:53:16 -0800

    Update

--- coding/Additional/playground.py
+++ coding/Additional/playground.py
@@ -21,3 +21,51 @@ test_metadata = {'Sampling frequency': 3303,
 extracted_metadata = rrc.extract_metadata_from_csv(test_file_path)
 print(extracted_metadata)
 
+
+
+
+
+
+
+
+
+
+
+
+
+def process_csv_folder(folder_path):
+    """
+    The function processes all CSV files in a folder and returns a list of reshaped data for each file.
+    """
+    # Get a list of all files in the folder
+    all_files = os.listdir(folder_path)
+
+    # Filter out only CSV files
+    csv_files = [file for file in all_files if file.endswith('.csv')]
+
+    # Initialize empty lists to store reshaped data
+    reshaped_data_list = []
+
+    # Loop through each CSV file
+    for csv_file in csv_files:
+        # Construct the full path to the CSV file
+        file_path = os.path.join(folder_path, csv_file)
+
+        # Read CSV file
+        x_positions, y_positions, u_velocities, v_velocities = read_csv_file(file_path)
+
+        # Reshape CSV file
+        x_grid, y_grid, u_grid, v_grid = reshape_csv_file(x_positions, y_positions, u_velocities, v_velocities)
+
+        # You can do more processing here
+
+        # Append reshaped data to the list
+        reshaped_data_list.append((x_grid, y_grid, u_grid, v_grid))
+
+    return reshaped_data_list
+
+# Example usage
+# folder_path = '/path/to/your/csv/files/'
+# reshaped_data_list = process_csv_folder(folder_path)
+
+# Now, reshaped_data_list contains a list of tuples, each containing the reshaped data from one CSV file
