6451af09510d66d99e2aba6a9c7c35d41031be2e
Julio Chavez <julio.chavez.dorado@gmail.com>
2023-12-02 22:33:08 -0800

    Add doctring to tests

--- src/Additional/test_read_and_reshape_csv.py
+++ src/Additional/test_read_and_reshape_csv.py
@@ -3,20 +3,30 @@ import tempfile
 import shutil
 import os
 import numpy as np
+import warnings
 import read_and_reshape_csv as rrc
 
 class TestReadAndReshapeCSV(unittest.TestCase):
 
+    test_directory = 'test_directory'
+
     def setUp(self):
-        # Create a temporary directory for testing
-        self.test_directory = 'test_directory'
-        os.makedirs(self.test_directory)
+        # Create a temporary directory for testing if it doesn't exist
+        if not os.path.exists(self.test_directory):
+            os.makedirs(self.test_directory)
 
     def tearDown(self):
-        # Remove the temporary directory and its contents after testing
-        os.rmdir(self.test_directory)
+        # Remove the files in the temporary directory
+        for file_name in os.listdir(self.test_directory):
+            file_path = os.path.join(self.test_directory, file_name)
+            os.remove(file_path)
 
+        # Remove the temporary directory
+        os.rmdir(self.test_directory)
     def test_extract_metadata_from_csv(self):
+        """
+        Test that the function extracts metadata from a CSV file.
+        """
         # Create a temporary CSV file with metadata
         csv_file_path = os.path.join(self.test_directory, 'test_file.csv')
         with open(csv_file_path, 'w') as file:
@@ -45,14 +55,24 @@ class TestReadAndReshapeCSV(unittest.TestCase):
         self.assertDictEqual(metadata, expected_metadata)
 
     def test_extract_and_check_consecutive_numbers(self):
-        # Create a temporary directory with consecutive frame-numbered CSV files
-        for i in range(1, 6):
-            file_path = os.path.join(self.test_directory, f'frame_{i}.csv')
-            with open(file_path, 'w') as file:
-                pass
+        """
+        Test that the function extracts frame numbers from CSV file names 
+        in a directory and checks if the numbers are consecutive.
+        """
+        # Create a temporary directory with non-consecutive frame-numbered CSV files
+        with open(os.path.join(self.test_directory, 'frame_1.csv'), 'w'):
+            pass
+        with open(os.path.join(self.test_directory, 'frame_3.csv'), 'w'):
+            pass
 
-        with self.assertWarns(Warning):
+        with warnings.catch_warnings(record=True) as w:
+            # Cause a warning by having non-consecutive frame numbers
             rrc.extract_and_check_consecutive_numbers(self.test_directory)
+
+            # Check that a warning was issued
+            self.assertEqual(len(w), 1)
+            self.assertTrue(issubclass(w[-1].category, Warning))
+            self.assertIn("Warning: The extracted numbers are not consecutive or there are missing numbers.", str(w[-1].message))
 
 
 if __name__ == '__main__':
