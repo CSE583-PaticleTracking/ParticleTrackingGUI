74503662e33b001e6fe98d9cff9e3ee9295e4949
Mohan Kukreja <mk44633@uw.edu>
2023-12-13 15:50:30 -0800

    GUI minor changes and adding opencv in environment

--- particlepals/gui_main.py
+++ particlepals/gui_main.py
@@ -3,16 +3,14 @@ streamlit test file
 """
 import os
 
+import numpy as np
+import matplotlib.pyplot as plt
 import streamlit as st
 
 # add package parent directory to sys path
 
-from Additional.read_and_reshape_csv import process_csv_folder
-
-## TODO: add particlepals imports
-# import our package/functions
-
-## TODO: check style and run tests
+from Additional.read_and_reshape_csv import process_csv_folder, read_csv_file, reshape_csv_file
+from Translation.PredictiveTracker import Predictive_tracker
 
 def main():
     """
@@ -27,13 +25,19 @@ def main():
     """
     # set page config
     st.set_page_config(layout="wide")
-    MediaFileStorageError = st.runtime.media_file_storage.MediaFileStorageError
+    # MediaFileStorageError = st.runtime.media_file_storage.MediaFileStorageError
     st.markdown('<p class="big-font">Particle Pals</p>', unsafe_allow_html=True)
     st.markdown(
                 '<p class="small-font">Particle Tracking and Vector Analysis Application</p>',
                 unsafe_allow_html=True
     )
     build_footer()
+
+    if 'button' not in st.session_state:
+        st.session_state.button = False
+
+    def click_button():
+        st.session_state.button = not st.session_state.button
 
     # Define sidebar data input
     data_path = st.sidebar.text_input("Data Path:", None)
@@ -76,31 +80,28 @@ def main():
         else:
             try:
                 data_file = get_file_name(data_path, data_extension)
-                dir_path = get_dir_path(data_path, data_file)
+                data_path_err.text(data_file)
+                ## implement this feature later...
+                # decided that for now we are going to assume .tif and .avi are in working dir.
+                # dir_path = get_dir_path(data_path, data_file)
             except (IndexError, ValueError, FileNotFoundError) as e:
                 data_path_err.text("Invalid input." +
                                    f"\nEncountered error: {e}." +
                                    "\nCheck path.")
 
-            dir_fd = os.open(dir_path, os.O_RDONLY)
-            def opener(path, flags):
-                return os.open(path, flags, dir_fd=dir_fd)
+            ## implement this feature later...
+            # decided that for now we are going to assume .tif and .avi are in working dir.
+            # dir_fd = os.open(dir_path, os.O_RDONLY)
+            # def opener(path, flags):
+            #     return os.open(path, flags, dir_fd=dir_fd)
             
             if data_extension == '.tif':
-                with open(data_file, 'rb', opener=opener) as f:
+                data_path = 'src\\' + data_file
+                # with open(data_file, 'rb', opener=opener) as f:
+                with open(data_path, 'rb') as f:
                     graphic_data = f.read()
                     graphic.image(graphic_data)
 
-        ## TODO: update graphic AFTER first radio button (based on analysis/file type)
-        # no longer matters because we are setting default index on radio button, anyway...
-        # try:
-        #     f = open(data_path, "rb")
-        #     image_data = f.read()
-        #     graphic.image(image_data)
-        # except MediaFileStorageError as e:
-        #     data_path_err.text("Invalid path!")
-        #     invalid_path = True
-        ## TODO: delete block above. leaving for now in case I wnat to reference
         if not invalid_path:
             # update instructions
             instructions.text("Populate input fields and execute computation.")
@@ -116,20 +117,62 @@ def main():
                 index=radio_index
             )
             if analysis_type == "Particle Tracking":
-                threshold = st.sidebar.number_input("Brightness Threshold")
-                max_disp = st.sidebar.number_input("Maximum Displacement")
-                min_area = st.sidebar.number_input("Minimum Displacement")
-                framerange = st.sidebar.slider("Framerange")
+                threshold = st.sidebar.number_input("Brightness Threshold", min_value=0)
+                max_disp = st.sidebar.number_input("Maximum Displacement", min_value=0)
+                min_area = st.sidebar.number_input("Minimum Displacement", min_value=0)
+                startframe = st.sidebar.number_input("Start Frame", step=1, min_value=0)
+                endframe = st.sidebar.number_input("End Frame", step=1, min_value=startframe+1)
+                # framerange = st.sidebar.slider("Framerange")
                 invert = st.sidebar.radio("Invert", ("Bright", "Dark"))
 
-                ## TODO: Implement submit button
-                # run other functions from particlepals package here,
-                # update inputs as required, and handle output
-                submit = st.sidebar.button("Compute")
+                framerange = np.arange(startframe, endframe)
+
+                match invert:
+                    case "Bright":
+                        invert = 0
+                    case "Dark":
+                        invert = 1
+
+                submit = st.sidebar.button("Compute", on_click=click_button)
+
+                if st.session_state.button:
+                    try:
+                        Predictive_tracker(
+                            inputnames=data_file,
+                            threshold=threshold,
+                            max_disp=max_disp,
+                            minarea=min_area,
+                            invert=invert,
+                            framerange=framerange,
+                            bground_name=None,
+                            noisy=None,
+                            gifname=None,
+                            found=None,
+                            correct=None,
+                            yesvels=None
+                        )
+                    except FileNotFoundError as e:
+                        st.sidebar.text(f"Computation raised error: \n{e}" +
+                                        "\nCheck inputs."
+                                        )
             else:
                 # for vector analysis, computation params are gathered from csv files in directory
                 # csv files should follow naming convention, but don't need to check for that here
-                ## TODO: add other inputs for VA
+                i = 0
+                data_file = f'frame_{i}.csv'
+                data_path = get_dir_path(data_path + data_file, 'frame_0.csv')
+
+                # update graphic
+                x_positions, y_positions, u_velocities, v_velocities = read_csv_file(os.path.join(data_path, data_file))
+                x_grid, y_grid, u_grid, v_grid = reshape_csv_file(x_positions, y_positions, u_velocities, v_velocities)
+                
+                fig = plt.figure(figsize=(4,4))
+                plt.quiver(x_grid, y_grid, u_grid, v_grid, scale=15, scale_units='xy', angles='xy', cmap='viridis')
+                plt.title(f'Turbulent Velocity Field')
+                plt.xlabel('X')
+                plt.ylabel('Y')
+                graphic.pyplot(fig,use_container_width=False)
+
                 operation = st.sidebar.radio("Operation",
                                             (
                                                 'add',
@@ -141,17 +184,49 @@ def main():
                                             )
                 )
 
+                vector_x = st.sidebar.number_input("Vector X")
+                vector_y = st.sidebar.number_input("Vector Y")
+                vector = (vector_x, vector_y)
+
                 # run other functions from particlepals package here,
                 # update inputs as required, and handle output
-                submit = st.sidebar.button("Compute")
+                submit = st.sidebar.button("Compute", on_click=click_button)
 
-                if submit:
+                if st.session_state.button:
+                    data_path_err.text(data_path)
                     try:
-                        process_csv_folder(data_path, operation)
+                        u_grid, v_grid, numbers = process_csv_folder(data_path, operation, vector)
+                        frame_num = st.slider('Frame', min_value=min(numbers), max_value=max(numbers))
+                        fig = plt.figure(figsize=(4,4))
+                        plt.quiver(x_grid, y_grid, u_grid, v_grid, scale=15, scale_units='xy', angles='xy', cmap='viridis')
+                        plt.title(f'Turbulent Velocity Field, Frame {frame_num}')
+                        plt.xlabel('X')
+                        plt.ylabel('Y')
+                        graphic.pyplot(fig,use_container_width=False)
                     except FileNotFoundError as e:
                         st.sidebar.text(f"Computation raised error: \n{e}" +
                                         "\nCheck inputs."
                                         )
+
+                # # run other functions from particlepals package here,
+                # # update inputs as required, and handle output
+                # submit = st.sidebar.button("Compute")
+
+                # if submit:
+                #     data_path_err.text(data_path)
+                #     try:
+                #         u_grid, v_grid, numbers = process_csv_folder(data_path, operation, vector)
+                #         frame_num = st.slider('Frame', min_value=min(numbers), max_value=max(numbers))
+                #         fig = plt.figure(figsize=(4,4))
+                #         plt.quiver(x_grid, y_grid, u_grid, v_grid, scale=15, scale_units='xy', angles='xy', cmap='viridis')
+                #         plt.title(f'Turbulent Velocity Field, Frame {frame_num}')
+                #         plt.xlabel('X')
+                #         plt.ylabel('Y')
+                #         graphic.pyplot(fig,use_container_width=False)
+                #     except FileNotFoundError as e:
+                #         st.sidebar.text(f"Computation raised error: \n{e}" +
+                #                         "\nCheck inputs."
+                #                         )
     else:
         pass
 
@@ -228,10 +303,13 @@ def get_file_name(path, extension):
     elif '\\' in path:
         slash_idx = path.rfind('\\')
         file_name = path[slash_idx+1:]
-
+    else:
+        # if path is just a file name, return it
+        file_name = path
     # if file name hasn't changed, let somebody know
     if file_name == '':
         raise ValueError("Could not get file name from path.")
+    
     return file_name
 
 
@@ -239,8 +317,8 @@ def get_dir_path(full_path, file):
     """
     Gets absolute path (excluding file name and extension) from full path.
     Inputs:
-        path: string containing path to file.
-            Can be absolute or relative.
+        path: string containing path to file plus file name and extension.
+            Path can be absolute or relative.
         file: file name and extension.
     Returns:
         dir_path: string absolute path to file's parent directory
@@ -249,20 +327,48 @@ def get_dir_path(full_path, file):
     dir_path = full_path[0:file_idx]
     is_relative_path = '.' in dir_path
     if not is_relative_path:
-        return full_path
-    
-    # replace relative references with absolute
-    cwd = os.getcwd()
-    while is_relative_path:
-        dot_idx = full_path.find('.')
+        return dir_path
+    else:
+        # replace relative references with absolute
+        cwd = os.getcwd()
+        dot_idx = dir_path.find('.')
 
-        # check if referencing current directory or parents
-        if full_path[dot_idx+1] == '.':
-            # if referencing parent directory, remove everything
-            # after second-to-last slash in absolute path
-            dir_path = dir
+        # first dot should always be at index zero...
+        if dot_idx != 0:
+            raise ValueError("Invalid path. Path should start with relative refs.")
 
- 
+        # check if referencing current directory ('.') or parent's ('..')
+        if '..' in dir_path:
+            # if path starts with '.':
+            if dir_path[dot_idx+1] != '.':
+                # since '.' is followed by '..', can simply remove it here...
+                # and we know that this is at index 0, followed by a single slash
+                dir_path = dir_path[2:]
+            # else:
+            #    pass
+
+            # now there is NO single dot but there is/are double(s)
+            # if referencing parent directory, count occurences
+            # then remove from abs path accordingly...
+            double_dot_count = dir_path.count('..')
+            for _ in range(double_dot_count):
+                remove_from_idx = cwd.rfind('\\')
+                cwd = cwd[:remove_from_idx]
+                remove_to_idx = dir_path.find('..')
+                dir_path = dir_path[remove_to_idx+2:]
+
+            # now construct new path by removing all dots and replacing with cwd
+            remove_to_idx = dir_path.rfind('..')
+            dir_path = cwd + dir_path[remove_to_idx:]
+
+        # if no parent references, simply replace single dot with cwd
+        else:
+            dir_path = dir_path.replace('.', cwd)
+
+        if '/' in dir_path:
+            dir_path = dir_path.replace('/','\\')
+
+        return dir_path
 
 
 def build_footer():
